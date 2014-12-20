44fbf46f23ddcd8d7b6d54e911bf622f1f2d8bf9
Gordon Watts <gordon@gordonandpaula.net>
2014-12-21 00:30:49 +0100

    Simple inclusion of MVVM light

---
+++ IWalker/IWalker.Windows/ViewModel/MvvmViewModel1.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace IWalker.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MvvmViewModel1 : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MvvmViewModel1 class.
+        /// </summary>
+        public MvvmViewModel1()
+        {
+        }
+    }
+}
