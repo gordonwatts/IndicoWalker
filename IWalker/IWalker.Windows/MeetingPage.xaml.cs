91417d5a01e371d67badbc92fc9407e4a9dc280f
Gordon Watts <gordon@gordonandpaula.net>
2015-01-03 22:40:06 +0100

    Moved meeting page to customize for each project.

---
+++ IWalker/IWalker.Windows/MeetingPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace IWalker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MeetingPage : Page, IViewFor<MeetingPageViewModel>
+    {
+        public MeetingPage()
+        {
+            this.InitializeComponent();
+
+            // Bind everything together we need.
+            this.Bind(ViewModel, x => x.MeetingTitle, y => y.MeetingTitle.Text);
+            this.BindCommand(ViewModel, x => x.HostScreen.Router.NavigateBack, y => y.backButton);
+        }
+
+        /// <summary>
+        /// Stash the view model
+        /// </summary>
+        public MeetingPageViewModel ViewModel
+        {
+            get { return (MeetingPageViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(MeetingPageViewModel), typeof(MeetingPage), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (MeetingPageViewModel)value; }
+        }
+    }
+}
