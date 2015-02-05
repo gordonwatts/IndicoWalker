2dd13c6cabc496ecdaa9ea6c4939c991c3d23827
Gordon Watts <gordon@gordonandpaula.net>
2015-02-06 00:23:35 +0100

    Experiments with SQLite - not in a buildable state!
    #60

--- IWalker/IWalker.Shared/DataModel/MRU/MRUDatabaseAccess.cs
+++ IWalker/IWalker.Shared/DataModel/MRU/MRUDatabaseAccess.cs
@@ -13,6 +13,19 @@ namespace IWalker.DataModel.MRU
     public class MRUDatabaseAccess : IMRUDatabase
     {
         /// <summary>
+        /// Reference to our db class.
+        /// </summary>
+        SQLiteDb _db = null;
+
+        /// <summary>
+        /// Connect up to our MRU database.
+        /// </summary>
+        public MRUDatabaseAccess()
+        {
+            _db = Model.DB;
+        }
+
+        /// <summary>
         /// Record this guy as having been visited.
         /// </summary>
         /// <param name="meeting"></param>
@@ -20,6 +33,15 @@ namespace IWalker.DataModel.MRU
         /// <param name="title"></param>
         public async Task MarkVisitedNow(IMeeting m)
         {
+            var c = new SQLite.SQLiteAsyncConnection("dude");
+            var mru = new IWalker.MRU()
+            {
+                IDRef = "no way",
+                StartTime = m.StartTime,
+                Title = m.Title
+            };
+            var r = await c.InsertAsync(mru);
+
             await Task.Delay(100); // Simulate the write. :-)
             Debug.WriteLine("Marking meeting {0} as being looked at", m.Title);
         }
---
+++ IWalker/IWalker.Shared/DataModel/ModelHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IWalker.DataModel
+{
+    partial class Model
+    {
+        private static Lazy<SQLiteDb> _db = new Lazy<SQLiteDb>(() => new SQLiteDb("indico.db"));
+
+        /// <summary>
+        /// Return the singltone for the database model.
+        /// </summary>
+        public static SQLiteDb DB
+        {
+            get { return _db.Value; }
+        }
+    }
+}
