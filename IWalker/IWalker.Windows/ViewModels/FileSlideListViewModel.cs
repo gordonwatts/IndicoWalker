98ae52996209a9f350db1ffe3a56986537aeaad1
Gordon Watts <gordon@gordonandpaula.net>
2015-02-18 15:32:03 +0100

    First basic testing for the file image display
    #151

--- IWalker/IWalker.Windows/ViewModels/FileSlideListViewModel.cs
+++ IWalker/IWalker.Windows/ViewModels/FileSlideListViewModel.cs
@@ -4,10 +4,13 @@ using IWalker.Util;
 using ReactiveUI;
 using Splat;
 using System;
+using System.IO;
 using System.Diagnostics;
 using System.Linq;
 using System.Reactive.Linq;
 using Windows.Data.Pdf;
+using Windows.Storage;
+using System.Reactive;
 namespace IWalker.ViewModels
 {
     /// <summary>
@@ -24,6 +27,11 @@ namespace IWalker.ViewModels
         /// The list of thumbnails
         /// </summary>
         public ReactiveList<SlideThumbViewModel> SlideThumbnails { get; private set; }
+
+        /// <summary>
+        /// An observable that will fire each time we update the images.
+        /// </summary>
+        public IObservable<Unit> DoneBuilding { get; private set; }
 
         /// <summary>
         /// Setup the VM for the associated file.
@@ -49,15 +57,35 @@ namespace IWalker.ViewModels
                 // TODO: Replace the catch below to notify bad PDF format.
                 Exception userBomb;
                 var renderPDF = ReactiveCommand.CreateAsyncObservable(_ => f.GetFile(true));
-                renderPDF
-                    .SelectMany(sf => PdfDocument.LoadFromFileAsync(sf))
+                var pages = renderPDF
+                    .SelectMany(async sf =>
+                    {
+                        try
+                        {
+                            var d = await PdfDocument.LoadFromStreamAsync(sf);
+                            return d;
+                        }
+                        catch (Exception e)
+                        {
+                            //TODO surface these errors?
+                            Debug.WriteLine(string.Format("Error rendering PDF document: '{0}'", e.Message));
+                            return null;
+                        }
+                    })
                     .Do(doc => fullVM = new Lazy<FullTalkAsStripViewModel>(() => new FullTalkAsStripViewModel(Locator.Current.GetService<IScreen>(), doc)))
                     .Select(sf => Enumerable.Range(0, (int)sf.PageCount)
                                     .Select(index => Tuple.Create(index, sf.GetPage((uint)index)))
                                     .Select(p => new SlideThumbViewModel(p.Item2, fullVM, p.Item1)))
+                    .Publish();
+
+                pages
                     .ObserveOn(RxApp.MainThreadScheduler)
-                    .Subscribe(pages => SlideThumbnails.AddRange(pages),
+                    .Subscribe(pgs => SlideThumbnails.AddRange(pgs),
                                ex => userBomb = ex);
+
+                DoneBuilding = pages.Select(_ => Unit.Default);
+
+                pages.Connect();
 
                 // TODO: Normally this would not kick off a download, but in this case
                 // we will until we get a real background store in there. Then we can kick this
---
+++ Test_MRUDatabase/ViewModels/t_FileSlideListViewModel.cs
@@ -0,0 +1,73 @@
+﻿using Akavache;
+using IWalker.DataModel.Interfaces;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IWalker.ViewModels;
+using System.IO;
+using System.Reactive.Linq;
+
+namespace Test_MRUDatabase.ViewModels
+{
+    [TestClass]
+    public class t_FileSlideListViewModel
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            BlobCache.ApplicationName="Test_MRUDatabase";
+            BlobCache.UserAccount.InvalidateAll();
+            BlobCache.UserAccount.Flush();
+        }
+
+        [TestMethod]
+        public async Task TestNumberTimesFileRequested()
+        {
+            // Make sure the # of times a file is loaded from disk is reasonable.
+            var df = new dummmyFile("test.pdf", "test.pdf");
+            var vm = new FileSlideListViewModel(df);
+
+            var list = vm.SlideThumbnails;
+            Assert.IsNotNull(list);
+            Assert.AreEqual(0, list.Count);
+
+            await vm.DoneBuilding.FirstAsync();
+
+            Assert.AreEqual(10, list.Count);
+            Assert.AreEqual(1, df.Called);
+        }
+
+        // A dummmy file.
+        class dummmyFile : IFile
+        {
+            public int Called { get; private set; }
+            private string _name;
+            private string _url;
+            public dummmyFile(string url, string name)
+            {
+                _name = name;
+                _url = url;
+                Called = 0;
+            }
+
+            public bool IsValid { get { return true; } }
+
+            public string FileType { get { return "pdf"; } }
+
+            public string UniqueKey { get { return _name; } }
+
+            public async Task<StreamReader> GetFileStream()
+            {
+                Called++;
+                var f = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFileAsync(_url);
+                var reader = await f.OpenStreamForReadAsync();
+                return new StreamReader(reader);
+            }
+
+            public string DisplayName { get { return _name; } }
+        }
+    }
+}
