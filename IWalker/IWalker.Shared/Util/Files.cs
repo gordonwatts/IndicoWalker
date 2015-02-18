5349366da1a852c60b413aaef06affc957663c56
Gordon Watts <gordon@gordonandpaula.net>
2015-02-18 11:31:38 +0100

    A simple RO stream that works over a byte array
    This is necessary for #154

--- IWalker/IWalker.Shared/Util/Files.cs
+++ IWalker/IWalker.Shared/Util/Files.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace IWalker.Util
 {
     /// <summary>
     /// Help with all things having to do with files.
     /// </summary>
-    static class Files
+    public static class Files
     {
         /// <summary>
         /// Configure a file picker for certificate files we can understand.
@@ -25,6 +27,147 @@ namespace IWalker.Util
 
             return op;
         }
+
+        /// <summary>
+        /// Return the byte array as a read only buffer
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static IRandomAccessStream AsRORAByteStream (this byte[] buffer)
+        {
+            return new InMemoryRORAByteArrayStream(buffer);
+        }
+
+        class InMemoryRORAByteArrayStream : IRandomAccessStream
+        {
+            private  byte[] _buffer;
+            private ulong _position;
+
+            public InMemoryRORAByteArrayStream(byte[] buffer, ulong position = 0)
+            {
+                _buffer = buffer;
+                _position = position;
+                CheckPosition();
+            }
+
+            /// <summary>
+            /// Make sure the current EOF position is ok.
+            /// </summary>
+            private void CheckPosition()
+            {
+                if (_position > (ulong)_buffer.Length)
+                {
+                    _position = (ulong) _buffer.Length;
+                }
+            }
+
+            private ulong DistTillEnd()
+            {
+                return ((ulong) _buffer.Length) - _position;
+            }
+
+            public bool CanRead
+            {
+                get { return true; }
+            }
+
+            public bool CanWrite
+            {
+                get { return false; }
+            }
+
+            public IRandomAccessStream CloneStream()
+            {
+                return new InMemoryRORAByteArrayStream(_buffer, _position);
+            }
+
+            public IInputStream GetInputStreamAt(ulong position)
+            {
+                return new InMemoryRORAByteArrayStream(_buffer, position);
+            }
+
+            /// <summary>
+            /// Fail because we don't do writing.
+            /// </summary>
+            /// <param name="position"></param>
+            /// <returns></returns>
+            public IOutputStream GetOutputStreamAt(ulong position)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ulong Position
+            {
+                get { return _position; }
+            }
+
+            public void Seek(ulong position)
+            {
+                _position = position;
+                CheckPosition();
+            }
+
+            public ulong Size
+            {
+                get
+                {
+                    return (ulong) _buffer.Length;
+                }
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public void Dispose()
+            {
+            }
+
+            /// <summary>
+            /// Read back some number of bytes from our byte array into the buffer.
+            /// </summary>
+            /// <param name="buffer"></param>
+            /// <param name="count"></param>
+            /// <param name="options"></param>
+            /// <returns></returns>
+            public Windows.Foundation.IAsyncOperationWithProgress<IBuffer, uint> ReadAsync(IBuffer buffer, uint count, InputStreamOptions options)
+            {
+                var goodCount = (int) Math.Min(DistTillEnd(), (ulong) count);
+                return AsyncInfo.Run<IBuffer, uint>(async (token, progress) =>
+                {
+                    if (goodCount == 0)
+                        return null;
+                    using (var dw = new DataWriter())
+                    {
+                        var b = _buffer.AsBuffer((int) _position, goodCount);
+                        dw.WriteBuffer(b);
+                        var result = dw.DetachBuffer();
+                        _position += (ulong) goodCount;
+                        return result;
+                    }
+                });
+            }
+
+            /// <summary>
+            /// Don't support writing, so don't support this guy!
+            /// </summary>
+            /// <returns></returns>
+            public Windows.Foundation.IAsyncOperation<bool> FlushAsync()
+            {
+                throw new NotImplementedException();
+            }
+
+            /// <summary>
+            /// We don't support writing.
+            /// </summary>
+            /// <param name="buffer"></param>
+            /// <returns></returns>
+            public Windows.Foundation.IAsyncOperationWithProgress<uint, uint> WriteAsync(IBuffer buffer)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
 
     }
 }
---
+++ Test_MRUDatabase/Streams.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IWalker.Util;
+using System.IO;
+
+namespace Test_MRUDatabase
+{
+    [TestClass]
+    public class Streams
+    {
+        [TestMethod]
+        public async Task ByteSteamCopyTo()
+        {
+            byte[] data = new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            var raStream = data.AsRORAByteStream();
+            var ms = new MemoryStream();
+            await raStream.AsStream().CopyToAsync(ms);
+            Assert.AreEqual(10, ms.Length);
+            ms.Seek(5, SeekOrigin.Begin);
+            var d = ms.ReadByte();
+            Assert.AreEqual(d, 5);
+        }
+
+        [TestMethod]
+        public async Task ByteStreamCloneAndCopy()
+        {
+            byte[] data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var raStream = data.AsRORAByteStream();
+
+            var s2 = raStream.CloneStream();
+            s2.Seek(5);
+            
+            var ms1 = new MemoryStream();
+            var ms2 = new MemoryStream();
+
+            await raStream.AsStream().CopyToAsync(ms1);
+            await s2.AsStream().CopyToAsync(ms2);
+
+            ms1.Seek(0, SeekOrigin.Begin);
+            ms2.Seek(0, SeekOrigin.Begin);
+
+            var d = ms1.ReadByte();
+            Assert.AreEqual(0, d);
+
+            d = ms2.ReadByte();
+            Assert.AreEqual(5, d);
+        }
+    }
+}
