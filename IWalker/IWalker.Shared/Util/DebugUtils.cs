e3899abdda5628cd6cfa1be1706015c456f55777
Gordon Watts <gordon@gordonandpaula.net>
2015-02-21 20:38:47 +0100

    Simple debug util to help with printing diagnostic messages

---
+++ IWalker/IWalker.Shared/Util/DebugUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reactive.Linq;
+using System.Diagnostics;
+
+namespace IWalker.Util
+{
+    public static class DebugUtils
+    {
+        /// <summary>
+        /// Helper to write a debug message
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IObservable<T> WriteLine<T>(this IObservable<T> source, string message, params object[] args)
+        {
+            return source.Do(x => Debug.WriteLine(message, args));
+        }
+    }
+}
