3cf63bc474f395cd0c52815fd065c29cdef6895e
Gordon Watts <gordon@gordonandpaula.net>
2015-02-21 18:11:18 +0100

    Seperate tests to understand Akavache

---
+++ Test_MRUDatabase/dummyMeeting.cs
@@ -0,0 +1,115 @@
+﻿using IWalker.DataModel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_MRUDatabase
+{
+    public class MeetingHelpers
+    {
+
+        /// <summary>
+        /// Generate a meeting.
+        /// </summary>
+        /// <returns></returns>
+        public static dummyMeetingRef CreateMeeting()
+        {
+            return new dummyMeetingRef();
+        }
+    }
+
+    class dummyMeeting : IMeeting
+    {
+        public string Title
+        {
+            get { return "Meeting1"; }
+        }
+
+        public ISession[] Sessions
+        {
+            get { return new ISession[] { new dummySession() }; }
+        }
+
+        public DateTime StartTime { get; set; }
+
+        public string AsReferenceString()
+        {
+            return "meeting1";
+        }
+    }
+
+    class dummySession : ISession
+    {
+        public ITalk[] Talks
+        {
+            get { return new ITalk[] { new dummyTalk() }; }
+        }
+    }
+
+    class dummyTalk : ITalk
+    {
+
+        public string Title
+        {
+            get { return "talk 1"; }
+        }
+
+        public IFile TalkFile
+        {
+            get { return new dummyFile(); }
+        }
+    }
+
+    class dummyFile : IFile
+    {
+        public bool IsValid
+        {
+            get { return true; }
+        }
+
+        public string FileType
+        {
+            get { return "pdf"; }
+        }
+
+        public string UniqueKey
+        {
+            get { return "talk1.pdf"; }
+        }
+
+        public Task<System.IO.StreamReader> GetFileStream()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DisplayName
+        {
+            get { return "talk1.pdf"; }
+        }
+    }
+
+    /// <summary>
+    /// A pretty simple dummy meeting.
+    /// </summary>
+    public class dummyMeetingRef : IMeetingRef
+    {
+        public dummyMeetingRef()
+        {
+            NumberOfTimesFetched = 0;
+        }
+        public Task<IMeeting> GetMeeting()
+        {
+            NumberOfTimesFetched++;
+            return Task.Factory.StartNew(() => new dummyMeeting() as IMeeting);
+        }
+
+        public string AsReferenceString()
+        {
+            return "meeting";
+        }
+
+        public int NumberOfTimesFetched { get; set; }
+    }
+}
---
+++ Test_MRUDatabase/t_Akavache.cs
@@ -0,0 +1,90 @@
+﻿using Akavache;
+using IWalker.DataModel.Interfaces;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Newtonsoft.Json;
+using Splat;
+using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace Test_MRUDatabase
+{
+    /// <summary>
+    /// There are some behaviors in Akavache that were rather confusing.
+    /// This class contains some methods to make sure things work as we
+    /// are expecting them to.
+    /// </summary>
+    [TestClass]
+    public class t_Akavache
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            BlobCache.ApplicationName = "Test_MRUDatabase";
+            BlobCache.UserAccount.InvalidateAll();
+            BlobCache.UserAccount.Flush();
+            Locator.CurrentMutable.Register(() => new JsonSerializerSettings()
+            {
+                ObjectCreationHandling = ObjectCreationHandling.Replace,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.All,
+            }, typeof(JsonSerializerSettings), null);
+
+        }
+
+        [TestMethod]
+        public async Task GetAndFetchLatestWithItemsInThere()
+        {
+            var settings = Locator.Current.GetService<JsonSerializerSettings>();
+            Assert.IsNotNull(settings);
+            Assert.AreEqual(settings.TypeNameHandling, TypeNameHandling.All);
+
+            var m = new dummyMeetingRef();
+            Func<Task<IMeeting>> fetcher = async () =>
+            {
+                var x = await m.GetMeeting();
+                return x;
+            };
+
+            await BlobCache.UserAccount.InsertObject(m.AsReferenceString(), await m.GetMeeting());
+
+            var blob = BlobCache.UserAccount.GetAndFetchLatest(m.AsReferenceString(), fetcher);
+
+            var mtg = await blob
+                .ToList()
+                .FirstAsync();
+
+            Assert.IsNotNull(mtg);
+            Assert.AreEqual(2, mtg.Count);
+            Assert.IsNotNull(mtg.First());
+            Assert.IsNotNull(mtg.Skip(1).First());
+        }
+
+        [TestMethod]
+        public async Task GetAndFetchLatestWithNoItemsInThere()
+        {
+            var settings = Locator.Current.GetService<JsonSerializerSettings>();
+            Assert.IsNotNull(settings);
+            Assert.AreEqual(settings.TypeNameHandling, TypeNameHandling.All);
+
+            var m = new dummyMeetingRef();
+            Func<Task<IMeeting>> fetcher = async () =>
+            {
+                var x = await m.GetMeeting();
+                return x;
+            };
+
+            var blob = BlobCache.UserAccount.GetAndFetchLatest(m.AsReferenceString(), fetcher);
+
+            var mtg = await blob
+                .ToList()
+                .FirstAsync();
+
+            Assert.IsNotNull(mtg);
+            Assert.AreEqual(1, mtg.Count);
+            Assert.IsNotNull(mtg.First());
+        }
+
+    }
+}
