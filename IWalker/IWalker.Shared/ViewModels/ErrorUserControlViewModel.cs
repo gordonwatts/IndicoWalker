4a01b5f544615a2dd3a461d62a63156df6c78080
Gordon Watts <gordon@gordonandpaula.net>
2015-03-31 23:24:26 +0200

    First go at a VM for errors
    #244

---
+++ IWalker/IWalker.Shared/ViewModels/ErrorUserControlViewModel.cs
@@ -0,0 +1,63 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reactive.Linq;
+
+namespace IWalker.ViewModels
+{
+    /// <summary>
+    /// Simple VM for an error. We are represented by some small
+    /// button that comes visible only when there is an error, and then
+    /// we generate a formatted string to show what the error is
+    /// when the user wants to see it.
+    /// </summary>
+    public class ErrorUserControlViewModel : ReactiveObject
+    {
+        /// <summary>
+        /// Get if we've seen an error yet
+        /// </summary>
+        public bool ErrorSeen
+        {
+            get { return _haveSeenError.Value; }
+        }
+        private ObservableAsPropertyHelper<bool> _haveSeenError;
+
+        /// <summary>
+        /// Each time this fires, an error should be displayed (somehow).
+        /// </summary>
+        public IObservable<string> DisplayErrors { get; private set; }
+
+        /// <summary>
+        /// Fire this if you want to have all errors replayed.
+        /// </summary>
+        public ReactiveCommand<object> ViewRequest { get; private set; }
+
+        /// <summary>
+        /// Create, with a stream of errors. By default it only holds onto the most recent error.
+        /// </summary>
+        /// <param name="errors"></param>
+        public ErrorUserControlViewModel (IObservable<Exception> errors)
+        {
+            // When an error shows up...
+            errors
+                .Select(_ => true)
+                .ToProperty(this, x => x.ErrorSeen, out _haveSeenError, false);
+
+            // Cache the error stream as its shows up.
+            var bld = new StringBuilder();
+            errors
+                .Subscribe(e =>
+                {
+                    bld.Clear();
+                    bld.Append(e.Message);
+                });
+
+            // Show an error when it goes by
+            ViewRequest = ReactiveCommand.Create();
+            DisplayErrors = ViewRequest
+                .Select(_ => bld.ToString())
+                .Where(msg => !string.IsNullOrWhiteSpace(msg));
+        }
+    }
+}
---
+++ Test_MRUDatabase/ViewModels/t_ErrorUserControLViewModel.cs
@@ -0,0 +1,84 @@
+﻿using IWalker.ViewModels;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Reactive.Subjects;
+
+namespace Test_MRUDatabase.ViewModels
+{
+    /// <summary>
+    /// Test out the errors
+    /// </summary>
+    [TestClass]
+    public class t_ErrorUserControLViewModel
+    {
+        [TestMethod]
+        public void PlainCTor()
+        {
+            var errors = new Subject<Exception>();
+            var e = new ErrorUserControlViewModel(errors);
+        }
+
+        [TestMethod]
+        public void SeeErrorWhenNothingThere()
+        {
+            var errors = new Subject<Exception>();
+            var e = new ErrorUserControlViewModel(errors);
+            var dummy = e.ErrorSeen;
+            var errorString = "";
+            e.DisplayErrors.Subscribe(msg => errorString = msg);
+            e.ViewRequest.Execute(null);
+            Assert.AreEqual("", errorString);
+        }
+
+        [TestMethod]
+        public void NoErrorWhenSendOne()
+        {
+            var errors = new Subject<Exception>();
+            var e = new ErrorUserControlViewModel(errors);
+            var dummy = e.ErrorSeen;
+            var errorString = "";
+            e.DisplayErrors.Subscribe(msg => errorString = msg);
+            errors.OnNext(new ArgumentException("bogus"));
+            Assert.AreEqual("", errorString);
+        }
+
+        [TestMethod]
+        public void ErrorWhenSendOne()
+        {
+            var errors = new Subject<Exception>();
+            var e = new ErrorUserControlViewModel(errors);
+            var dummy = e.ErrorSeen;
+            var errorString = "";
+            e.DisplayErrors.Subscribe(msg => errorString = msg);
+            errors.OnNext(new ArgumentException("bogus"));
+            e.ViewRequest.Execute(null);
+            Assert.AreEqual("bogus", errorString);
+        }
+
+        [TestMethod]
+        public void ErrorWhenSendOneNoActivateErrorSeen()
+        {
+            var errors = new Subject<Exception>();
+            var e = new ErrorUserControlViewModel(errors);
+            var errorString = "";
+            e.DisplayErrors.Subscribe(msg => errorString = msg);
+            errors.OnNext(new ArgumentException("bogus"));
+            e.ViewRequest.Execute(null);
+            Assert.AreEqual("bogus", errorString);
+        }
+
+        [TestMethod]
+        public void LastErrorWhenSendTwo()
+        {
+            var errors = new Subject<Exception>();
+            var e = new ErrorUserControlViewModel(errors);
+            var dummy = e.ErrorSeen;
+            var errorString = "";
+            e.DisplayErrors.Subscribe(msg => errorString = msg);
+            errors.OnNext(new ArgumentException("bogus"));
+            errors.OnNext(new ArgumentException("help"));
+            e.ViewRequest.Execute(null);
+            Assert.AreEqual("help", errorString);
+        }
+    }
+}
