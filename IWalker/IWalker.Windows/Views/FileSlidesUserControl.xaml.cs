fabd9cc410478b4c48a48a5d2d0a8209070203f3
Gordon Watts <gordon@gordonandpaula.net>
2015-04-06 00:50:39 +0200

    Fix up to make it clear between thumbs and full slides
    Really should make naming more straightforward.

--- IWalker/IWalker.Windows/Views/FileSlidesUserControl.xaml.cs
+++ IWalker/IWalker.Windows/Views/FileSlidesUserControl.xaml.cs
@@ -8,7 +8,7 @@ using Windows.UI.Xaml.Controls;
 namespace IWalker.Views
 {
     /// <summary>
-    /// The view for the file slides. We are mostly a list of all the slide images.
+    /// The view for the file slides. We are mostly a list of all the slide thumbnails.
     /// </summary>
     public sealed partial class FileSlidesUserControl : UserControl, IViewFor<FileSlideListViewModel>
     {
