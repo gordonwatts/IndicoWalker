fe9c7a9e81f427d4eeda5faae24df6900e21a1f3
Gordon Watts <gordon@gordonandpaula.net>
2015-04-06 01:04:33 +0200

    Refactoring changes to get this running on WP
    Moved a bunch of WS only views back to the common view model. Created
    one dummy WP view. Very nicely done, actually. :-)
    fixes #136

---
+++ IWalker/IWalker.WindowsPhone/ViewModels/FileSlideListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ReactiveUI;
+
+namespace IWalker.ViewModels
+{
+    /// <summary>
+    /// Dummy VM for use building x-platform.
+    /// </summary>
+    public class FileSlideListViewModel : ReactiveObject
+    {
+        public FileSlideListViewModel(DataModel.Interfaces.IFile file, Util.TimePeriod timeSpan, System.IObservable<System.Reactive.Unit> observable)
+        {
+        }
+    }
+}
---
+++ IWalker/IWalker.WindowsPhone/Views/FileSlidesUserControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using IWalker.ViewModels;
+using ReactiveUI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace IWalker.Views
+{
+    public sealed partial class FileSlidesUserControl : UserControl, IViewFor<FileSlideListViewModel>
+    {
+        public FileSlidesUserControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Stash the view model
+        /// </summary>
+        public FileSlideListViewModel ViewModel
+        {
+            get { return (FileSlideListViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(FileSlideListViewModel), typeof(FileSlidesUserControl), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = (FileSlideListViewModel)value; }
+        }
+    }
+}
