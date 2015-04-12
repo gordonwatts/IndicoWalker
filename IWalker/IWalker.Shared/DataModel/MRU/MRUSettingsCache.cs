deb5039f478661d9635dda06b744873515f6fee1
Gordon Watts <gordon@gordonandpaula.net>
2015-04-12 23:46:30 +0200

    Basics for saving MRU's accross devices
    #124
    THis is the low level stuff. Now needs to be properly hooked up

---
+++ IWalker/IWalker.Shared/DataModel/MRU/MRUSettingsCache.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Storage;
+
+namespace IWalker.DataModel.MRU
+{
+    /// <summary>
+    /// Store the last 20 meetings we've visited in a cache.
+    /// That way others can look and use it to keep them approximately
+    /// up to date.
+    /// </summary>
+    /// <remarks>
+    /// Eventually this should be replaced by a real system that will store everything in the cloud.
+    /// </remarks>
+    public class MRUSettingsCache
+    {
+        /// <summary>
+        /// WHat we use to access the remote MRU lists.
+        /// </summary>
+        private const string RemoteMRUSettingsName = "RemoteMRULists";
+
+        /// <summary>
+        /// Grab the MRU lists from all other machines, combine them, and return them.
+        /// It does make sure that all returned lists contain no duplicates.
+        /// </summary>
+        /// <returns></returns>
+        public static IWalker.MRU[] GetAllMachineMRUMeetings()
+        {
+            var settings = GetOrCreateSettingsContainer();
+            var allMeetings = settings.Values.Keys
+                .Select(k => settings.Values[k] as string)
+                .SelectMany(json => JsonConvert.DeserializeObject<IWalker.MRU[]>(json));
+
+            var latestMeetings = allMeetings
+                .GroupBy(m => m.IDRef)
+                .Select(grp => grp.OrderByDescending(m => m.LastLookedAt).First());
+
+            return latestMeetings.ToArray();
+        }
+
+        /// <summary>
+        /// Get the data from a single machine
+        /// </summary>
+        /// <param name="machineName">Name of the machine from which to fetch the data</param>
+        /// <returns>The list of MRU's for the given machine or null</returns>
+        public static IWalker.MRU[] GetFromMachine(string machineName)
+        {
+            var settings = GetOrCreateSettingsContainer();
+            if (!settings.Values.ContainsKey(machineName))
+                return null;
+            var json = settings.Values[machineName] as string;
+            return JsonConvert.DeserializeObject<IWalker.MRU[]>(json);
+        }
+
+        /// <summary>
+        /// Save everything for a particular machine
+        /// </summary>
+        /// <param name="machineName">Name of the machine we should store or update this list</param>
+        /// <param name="mrus">List of MRU's to store</param>
+        public static void UpdateForMachine(string machineName, IWalker.MRU[] mrus)
+        {
+            var settings = GetOrCreateSettingsContainer();
+            settings.Values[machineName] = JsonConvert.SerializeObject(mrus);
+        }
+
+        /// <summary>
+        /// Returns the MRU settings container
+        /// </summary>
+        /// <returns></returns>
+        private static ApplicationDataContainer GetOrCreateSettingsContainer()
+        {
+            ApplicationData.Current.RoamingSettings.CreateContainer(RemoteMRUSettingsName, ApplicationDataCreateDisposition.Always);
+            return ApplicationData.Current.RoamingSettings.Containers[RemoteMRUSettingsName];
+        }
+
+        /// <summary>
+        /// Delete everything in the MRU cache - this is to help with testing.
+        /// </summary>
+        public static void ResetCache()
+        {
+            if (ApplicationData.Current.RoamingSettings.Containers.ContainsKey(RemoteMRUSettingsName))
+            {
+                ApplicationData.Current.RoamingSettings.DeleteContainer(RemoteMRUSettingsName);
+            }
+        }
+    }
+}
---
+++ Test_MRUDatabase/DataModel/MRU/t_MRUSettingsCache.cs
@@ -0,0 +1,132 @@
+﻿using IWalker.DataModel.MRU;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Linq;
+
+namespace Test_MRUDatabase.DataModel.MRU
+{
+    /// <summary>
+    /// test out the mru setting cache
+    /// </summary>
+    [TestClass]
+    public class t_MRUSettingsCache
+    {
+        [TestInitialize]
+        public void ResetCache()
+        {
+            MRUSettingsCache.ResetCache();
+        }
+
+        [TestMethod]
+        public void GetBackSpecificCacheEmpty()
+        {
+            var mrus = MRUSettingsCache.GetFromMachine("MACHINE1");
+            Assert.IsNull(mrus);
+        }
+
+        [TestMethod]
+        public void SetSpecificCache()
+        {
+            var mrus = GenerateMRUs();
+            MRUSettingsCache.UpdateForMachine("MACHINE1", mrus);
+            var rtn = MRUSettingsCache.GetFromMachine("MACHINE1");
+            Assert.AreEqual(mrus.Length, rtn.Length);
+            foreach (var dual in mrus.Zip(rtn, (m, r) => Tuple.Create(m, r)))
+            {
+                Assert.AreEqual(dual.Item1.Id, dual.Item2.Id);
+                Assert.AreEqual(dual.Item1.IDRef, dual.Item2.IDRef);
+                Assert.AreEqual(dual.Item1.LastLookedAt, dual.Item2.LastLookedAt);
+                Assert.AreEqual(dual.Item1.StartTime, dual.Item2.StartTime);
+                Assert.AreEqual(dual.Item1.Title, dual.Item2.Title);
+            }
+        }
+
+        [TestMethod]
+        public void UpdateSpecificCache()
+        {
+            var mrus = GenerateMRUs(5, 0);
+            MRUSettingsCache.UpdateForMachine("MACHINE1", mrus);
+            mrus = GenerateMRUs(10, 100);
+            MRUSettingsCache.UpdateForMachine("MACHINE1", mrus);
+            var rtn = MRUSettingsCache.GetFromMachine("MACHINE1");
+            Assert.AreEqual(10, rtn.Length);
+            Assert.AreEqual(100, rtn[0].Id);
+        }
+
+        [TestMethod]
+        public void NewDoesNotDeleteOld()
+        {
+            var mrus = GenerateMRUs(5);
+            MRUSettingsCache.UpdateForMachine("MACHINE1", mrus);
+            MRUSettingsCache.UpdateForMachine("MACHINE2", mrus);
+            var rtn = MRUSettingsCache.GetFromMachine("MACHINE1");
+            Assert.AreEqual(5, rtn.Length);
+        }
+
+        [TestMethod]
+        public void EmptyCache()
+        {
+            var mrus = MRUSettingsCache.GetAllMachineMRUMeetings();
+            Assert.AreEqual(0, mrus.Length);
+        }
+
+        [TestMethod]
+        public void SingleListInCache()
+        {
+            var mrus = GenerateMRUs();
+            MRUSettingsCache.UpdateForMachine("MACHINE1", mrus);
+            var rtn = MRUSettingsCache.GetAllMachineMRUMeetings();
+            Assert.AreEqual(10, rtn.Length);
+            Assert.AreEqual(mrus[0].IDRef, rtn[0].IDRef);
+        }
+
+        [TestMethod]
+        public void MergedListNothingCommon()
+        {
+            var mrus = GenerateMRUs();
+            MRUSettingsCache.UpdateForMachine("MACHINE1", mrus);
+            mrus = GenerateMRUs(10, 100);
+            MRUSettingsCache.UpdateForMachine("MACHINE2", mrus);
+            var rtn = MRUSettingsCache.GetAllMachineMRUMeetings();
+            Assert.AreEqual(20, rtn.Length);
+        }
+
+        [TestMethod]
+        public void MergedListCommonItems()
+        {
+            var mrus = GenerateMRUs();
+            MRUSettingsCache.UpdateForMachine("MACHINE1", mrus);
+            MRUSettingsCache.UpdateForMachine("MACHINE2", mrus);
+            var rtn = MRUSettingsCache.GetAllMachineMRUMeetings();
+            Assert.AreEqual(10, rtn.Length);
+        }
+
+        [TestMethod]
+        public void MergedListKeepRightOne()
+        {
+            var l1 = new IWalker.MRU[] {
+                new IWalker.MRU { Id=10, IDRef = "hiref", Title="this is it", StartTime=DateTime.Now, LastLookedAt=DateTime.Parse("1/20/2010")}
+            };
+            var l2 = new IWalker.MRU[] {
+                new IWalker.MRU { Id=10, IDRef = "hiref", Title="this is not it", StartTime=DateTime.Now, LastLookedAt=DateTime.Parse("1/20/2011")}
+            };
+            MRUSettingsCache.UpdateForMachine("M1", l1);
+            MRUSettingsCache.UpdateForMachine("M2", l2);
+
+            var l = MRUSettingsCache.GetAllMachineMRUMeetings();
+            Assert.AreEqual(1, l.Length);
+            Assert.AreEqual("this is not it", l[0].Title);
+        }
+
+        /// <summary>
+        /// Generate some MRU's to store.
+        /// </summary>
+        /// <returns></returns>
+        private IWalker.MRU[] GenerateMRUs(int number = 10, int initialID = 0)
+        {
+            return Enumerable.Range(initialID, number)
+                .Select(id => new IWalker.MRU { Id = id, IDRef = id.ToString(), LastLookedAt = DateTime.Now, StartTime = DateTime.Now, Title = string.Format("Meeting id {0}", id) })
+                .ToArray();
+        }
+    }
+}
