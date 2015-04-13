1047794234765704b8dd6db937ea2a4a5b70558e
Gordon Watts <gordon@gordonandpaula.net>
2015-04-13 23:50:10 +0200

    First go at the downloader object

---
+++ IWalker/IWalker.Shared/ViewModels/IFileDownloadController.cs
@@ -0,0 +1,70 @@
+﻿using Akavache;
+using IWalker.DataModel.Interfaces;
+using IWalker.Util;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace IWalker.ViewModels
+{
+    /// <summary>
+    /// Helps coordinate and control the downloading and updating of file (IFile) data
+    /// for the app. Shared amongst various VM's as necessary.
+    /// </summary>
+    /// <remarks>
+    /// There should only be one of these at any time for a particular file.
+    /// </remarks>
+    public class IFileDownloadController : ReactiveObject
+    {
+        /// <summary>
+        /// True if data is being downloaded as we speak
+        /// </summary>
+        public bool IsDownloading
+        {
+            get { return _isDownloading.Value; }
+        }
+        private ObservableAsPropertyHelper<bool> _isDownloading;
+
+        /// <summary>
+        /// True if there is data for this file available in the local cache
+        /// </summary>
+        /// <remarks>
+        /// This would be true even if there is an update available on the server, or
+        /// even if there was an update download in progress
+        /// </remarks>
+        public bool IsDownloaded
+        {
+            get { return _isDownloaded.Value; }
+        }
+        private ObservableAsPropertyHelper<bool> _isDownloaded;
+
+        /// <summary>
+        /// Create the download controller for this file
+        /// </summary>
+        /// <param name="file"></param>
+        public IFileDownloadController(IFile file, IBlobCache cache = null)
+        {
+            _file = file;
+            _cache = cache == null ? Blobs.LocalStorage : cache;
+
+            // Track the status of the download
+            _cache.GetCreatedAt(_file.UniqueKey)
+                .Select(dt => dt.HasValue)
+                .ToProperty(this, x => x.IsDownloaded, out _isDownloaded, false);
+        }
+
+        /// <summary>
+        /// The file we control
+        /// </summary>
+        private IFile _file;
+
+        /// <summary>
+        /// Cache used to load everything up
+        /// </summary>
+        private IBlobCache _cache;
+
+    }
+}
---
+++ Test_MRUDatabase/ViewModels/t_IFileDownloadController.cs
@@ -0,0 +1,117 @@
+﻿using Akavache;
+using IWalker.ViewModels;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_MRUDatabase.ViewModels
+{
+    [TestClass]
+    public class t_IFileDownloadController
+    {
+        [TestInitialize]
+        public void TestSetup()
+        {
+
+        }
+
+        [TestMethod]
+        public void NoFileDownloadedIsNotCached()
+        {
+            var f = new dummyFile();
+            var vm = new IFileDownloadController(f, new dummyCache());
+            Assert.IsFalse(vm.IsDownloaded);
+        }
+
+        [TestMethod]
+        public void FielDownlaodedIsCached()
+        {
+            var f = new dummyFile();
+            var d = new Dictionary<string, dummyCache.dummyCacheInfo>();
+            d[f.UniqueKey] = new dummyCache.dummyCacheInfo() { DateCreated = DateTime.Now };
+            var vm = new IFileDownloadController(f, new dummyCache(d));
+            
+            Assert.IsTrue(vm.IsDownloaded);
+        }
+
+        private class dummyCache : IBlobCache
+        {
+            private Dictionary<string, dummyCacheInfo> _lines;
+            public class dummyCacheInfo {
+                public DateTime DateCreated;
+            }
+
+            public dummyCache(Dictionary<string, dummyCacheInfo> lines = null)
+            {
+                _lines = lines;
+                if (_lines == null)
+                {
+                    _lines = new Dictionary<string, dummyCacheInfo>();
+                }
+            }
+            public IObservable<System.Reactive.Unit> Flush()
+            {
+                throw new NotImplementedException();
+            }
+
+            public IObservable<byte[]> Get(string key)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IObservable<IEnumerable<string>> GetAllKeys()
+            {
+                throw new NotImplementedException();
+            }
+
+            public IObservable<DateTimeOffset?> GetCreatedAt(string key)
+            {
+                if (!_lines.ContainsKey(key))
+                {
+                    return Observable.Return((DateTimeOffset?)null);
+                }
+                return Observable.Return<DateTimeOffset?>(_lines[key].DateCreated);
+            }
+
+            public IObservable<System.Reactive.Unit> Insert(string key, byte[] data, DateTimeOffset? absoluteExpiration = null)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IObservable<System.Reactive.Unit> Invalidate(string key)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IObservable<System.Reactive.Unit> InvalidateAll()
+            {
+                throw new NotImplementedException();
+            }
+
+            public System.Reactive.Concurrency.IScheduler Scheduler
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public IObservable<System.Reactive.Unit> Shutdown
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public IObservable<System.Reactive.Unit> Vacuum()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
