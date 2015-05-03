58f8fd7d56918c3bdab854e719f39dbef849b06c
Gordon Watts <gordon@gordonandpaula.net>
2015-05-03 22:50:23 +0200

    Start of a test to see what is going on with the download button

---
+++ Test_MRUDatabase/ViewModels/t_FileUserControlViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_MRUDatabase.ViewModels
+{
+    [TestClass]
+    public class t_FileUserControlViewModel
+    {
+        [TestMethod]
+        public async Task IsDownloadingSetCorrectly()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
