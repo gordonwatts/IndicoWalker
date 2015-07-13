bc47145d6e3f5fb68cd8b6064349282eac5318ed
Gordon Watts <gordon@gordonandpaula.net>
2015-07-13 02:39:19 +0200

    First 2-talk test
    #306
    Currently fails - so this is one bug as to why it isn't doing the open.

--- Test_MRUDatabase/ViewModels/t_ExpandingSlideThumbViewModel.cs
+++ Test_MRUDatabase/ViewModels/t_ExpandingSlideThumbViewModel.cs
@@ -2,7 +2,9 @@
 using IWalker.ViewModels;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 
@@ -14,23 +16,16 @@ namespace Test_MRUDatabase.ViewModels
     [TestClass]
     public class t_ExpandingSlideThumbViewModel
     {
+        /// <summary>
+        /// Make sure that the thing doesn't open on its own.
+        /// </summary>
+        /// <returns></returns>
         [TestMethod]
         public async Task ExpandDosentHappenAutomatically()
         {
             // Open a single talk and see if we can see it open.
             var now = new TimePeriod(DateTime.Now, DateTime.Now + TimeSpan.FromSeconds(1000));
-
-            // In this test we make sure not to access the # of pages.
-            var f = new dummyFile();
-            var data = await TestUtils.GetFileAsBytes("test.pdf");
-            f.GetStream = () =>
-            {
-                return Observable.Return(new StreamReader(new MemoryStream(data)));
-            };
-            var dc = new dummyCache();
-            var vm = new FileDownloadController(f, dc);
-
-            var pf = new PDFFile(vm);
+            var pf = (await MakeDownloaders(1))[0];
 
             var exp = new ExpandingSlideThumbViewModel(pf, now);
 
@@ -39,23 +34,47 @@ namespace Test_MRUDatabase.ViewModels
             Assert.IsNull(exp.TalkAsThumbs);
         }
 
+        /// <summary>
+        /// Generate a number of MakeDownloaders.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        private static async Task<PDFFile[]> MakeDownloaders(int count)
+        {
+            // In this test we make sure not to access the # of pages.
+
+            Func<Task<PDFFile>> creator = async () =>
+            {
+                var f = new dummyFile();
+                var data = await TestUtils.GetFileAsBytes("test.pdf");
+                f.GetStream = () =>
+                {
+                    return Observable.Return(new StreamReader(new MemoryStream(data)));
+                };
+                var dc = new dummyCache();
+                var vm = new FileDownloadController(f, dc);
+
+                return new PDFFile(vm);
+            };
+
+            List<PDFFile> r = new List<PDFFile>();
+            for (int i = 0; i < count; i++)
+            {
+                r.Add(await creator());
+            }
+            return r.ToArray();
+        }
+
+        /// <summary>
+        /// Make sure that with the given file downloaded, we can show slides.
+        /// </summary>
+        /// <returns></returns>
         [TestMethod]
         public async Task ShowSlidesIsAllowed()
         {
             // Open a single talk and see if we can see it open.
             var now = new TimePeriod(DateTime.Now, DateTime.Now + TimeSpan.FromSeconds(1000));
-
-            // In this test we make sure not to access the # of pages.
-            var f = new dummyFile();
-            var data = await TestUtils.GetFileAsBytes("test.pdf");
-            f.GetStream = () =>
-            {
-                return Observable.Return(new StreamReader(new MemoryStream(data)));
-            };
-            var dc = new dummyCache();
-            var vm = new FileDownloadController(f, dc);
-
-            var pf = new PDFFile(vm);
+            var pf = (await MakeDownloaders(1))[0];
 
             var exp = new ExpandingSlideThumbViewModel(pf, now);
 
@@ -63,23 +82,16 @@ namespace Test_MRUDatabase.ViewModels
             await TestUtils.SpinWait(() => exp.CanShowThumbs == true, 500);
         }
 
+        /// <summary>
+        /// Make sure we actually show the slides.
+        /// </summary>
+        /// <returns></returns>
         [TestMethod]
         public async Task ShowSlidesInOne()
         {
             // Open a single talk and see if we can see it open.
             var now = new TimePeriod(DateTime.Now, DateTime.Now + TimeSpan.FromSeconds(1000));
-
-            // In this test we make sure not to access the # of pages.
-            var f = new dummyFile();
-            var data = await TestUtils.GetFileAsBytes("test.pdf");
-            f.GetStream = () =>
-            {
-                return Observable.Return(new StreamReader(new MemoryStream(data)));
-            };
-            var dc = new dummyCache();
-            var vm = new FileDownloadController(f, dc);
-
-            var pf = new PDFFile(vm);
+            var pf = (await MakeDownloaders(1))[0];
 
             var exp = new ExpandingSlideThumbViewModel(pf, now);
 
@@ -91,6 +103,32 @@ namespace Test_MRUDatabase.ViewModels
 
             // See if they opened!
             await TestUtils.SpinWait(() => exp.TalkAsThumbs != null, 5000);
+        }
+
+        /// <summary>
+        /// WHen we open the second talk, the first one should be closed.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task ShowSlidesInSecondTalk()
+        {
+            var now = new TimePeriod(DateTime.Now, DateTime.Now + TimeSpan.FromSeconds(1000));
+            var pf = await MakeDownloaders(2);
+            var exps = pf.Select(pdf => new ExpandingSlideThumbViewModel(pdf, now)).ToArray();
+
+            // Initialize the download guys
+            Assert.IsNull(exps[0].TalkAsThumbs);
+            Assert.IsNull(exps[1].TalkAsThumbs);
+
+            // Open the first talk. Second talk should do nothing.
+            exps[0].ShowSlides.Execute(null);
+            await TestUtils.SpinWait(() => exps[0].TalkAsThumbs != null, 5000);
+            Assert.IsNull(exps[1].TalkAsThumbs);
+
+            // Open the second talk. First one should close.
+            exps[1].ShowSlides.Execute(null);
+            await TestUtils.SpinWait(() => exps[1].TalkAsThumbs != null, 5000);
+            await TestUtils.SpinWait(() => exps[0].TalkAsThumbs == null, 1000);
         }
 
 #if later
