20569a8f228d2666aadbfcc25a85ff9bd23659d6
Gordon Watts <gordon@gordonandpaula.net>
2015-07-18 02:27:54 +0200

    More careful boundry tests
    Discovered a crash here, an attempt to track it down. Not found
    (multi-threaded!?)

--- Test_MRUDatabase/Streams.cs
+++ Test_MRUDatabase/Streams.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+﻿using IWalker.Util;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using IWalker.Util;
 using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 
 namespace Test_MRUDatabase
 {
@@ -15,7 +13,7 @@ namespace Test_MRUDatabase
         [TestMethod]
         public async Task ByteSteamCopyTo()
         {
-            byte[] data = new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            byte[] data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             var raStream = data.AsRORAByteStream();
             var ms = new MemoryStream();
             await raStream.AsStream().CopyToAsync(ms);
@@ -26,6 +24,65 @@ namespace Test_MRUDatabase
         }
 
         [TestMethod]
+        public async Task Read1000WhenThereAre10()
+        {
+            byte[] data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var raStream = data.AsRORAByteStream();
+
+            byte[] destData = new byte[1024];
+            var destBuffer = destData.AsBuffer();
+
+            var r = await raStream.ReadAsync(destBuffer, 1000, Windows.Storage.Streams.InputStreamOptions.ReadAhead);
+
+            Assert.AreEqual((uint)10, r.Length);
+        }
+
+        [TestMethod]
+        public async Task ReadPartial5ReadThenFull()
+        {
+            byte[] data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var raStream = data.AsRORAByteStream();
+
+            byte[] destData = new byte[1024];
+            var destBuffer = destData.AsBuffer();
+
+            var r = await raStream.ReadAsync(destBuffer, 5, Windows.Storage.Streams.InputStreamOptions.None);
+            Assert.AreEqual((uint)5, r.Length);
+            r = await raStream.ReadAsync(destBuffer, 1000, Windows.Storage.Streams.InputStreamOptions.None);
+            Assert.AreEqual((uint)5, r.Length);
+        }
+
+        [TestMethod]
+        public async Task ReadPartial9ReadThenFull()
+        {
+            byte[] data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var raStream = data.AsRORAByteStream();
+
+            byte[] destData = new byte[1024];
+            var destBuffer = destData.AsBuffer();
+
+            var r = await raStream.ReadAsync(destBuffer, 9, Windows.Storage.Streams.InputStreamOptions.None);
+            Assert.AreEqual((uint)9, r.Length);
+            r = await raStream.ReadAsync(destBuffer, 1000, Windows.Storage.Streams.InputStreamOptions.None);
+            Assert.AreEqual((uint)1, r.Length);
+        }
+
+        [TestMethod]
+        public async Task ReadPartial10ReadThenFull()
+        {
+            byte[] data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var raStream = data.AsRORAByteStream();
+
+            byte[] destData = new byte[1024];
+            var destBuffer = destData.AsBuffer();
+
+            var r = await raStream.ReadAsync(destBuffer, 10, Windows.Storage.Streams.InputStreamOptions.None);
+            Assert.AreEqual((uint)10, r.Length);
+            r = await raStream.ReadAsync(destBuffer, 1000, Windows.Storage.Streams.InputStreamOptions.None);
+            Assert.IsNull(r);
+        }
+
+        [TestMethod]
         public async Task ByteStreamCloneAndCopy()
         {
             byte[] data = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
@@ -33,7 +90,7 @@ namespace Test_MRUDatabase
 
             var s2 = raStream.CloneStream();
             s2.Seek(5);
-            
+
             var ms1 = new MemoryStream();
             var ms2 = new MemoryStream();
 
