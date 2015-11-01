741122b71ff61bd37f25336edb351a3ac6c70dab
Gordon Watts <gordon@gordonandpaula.net>
2015-11-01 01:57:23 -0700

    Update tests I forgot to update
    #336
    That is embarassing. I forgot to run tests!

---
+++ Test_MRUDatabase/ViewModels/t_OpenURLControlViewModel.cs
@@ -0,0 +1,65 @@
+﻿using IWalker.ViewModels;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test_MRUDatabase.Util;
+
+namespace Test_MRUDatabase.ViewModels
+{
+    [TestClass]
+    public class t_OpenURLControlViewModel
+    {
+        [TestMethod]
+        public void LoadNormalMeeting()
+        {
+            var ds = new dummyScreen();
+            var t = new OpenURLControlViewModel(ds);
+
+            object newPage = null;
+            ds.Router.Navigate.Subscribe(o => newPage = o);
+
+            t.MeetingAddress = "https://indico.cern.ch/event/377091/";
+            t.SwitchPages.Execute(null);
+
+            Assert.IsNotNull(newPage);
+            Assert.IsInstanceOfType(newPage, typeof(MeetingPageViewModel));
+        }
+
+        [TestMethod]
+        public void LoadCategory()
+        {
+            var ds = new dummyScreen();
+            var t = new OpenURLControlViewModel(ds);
+
+            object newPage = null;
+            ds.Router.Navigate.Subscribe(o => newPage = o);
+
+            t.MeetingAddress = "https://indico.cern.ch/export/categ/1l12.ics?from=-7d";
+            t.SwitchPages.Execute(null);
+
+            Assert.IsNotNull(newPage);
+            Assert.IsInstanceOfType(newPage, typeof(CategoryPageViewModel));
+        }
+
+
+#if false
+        [TestMethod]
+        public void LoadBadURL()
+        {
+            var ds = new dummyScreen();
+            var t = new StartPageViewModel(ds);
+
+            object newPage = null;
+            ds.Router.Navigate.Subscribe(o => newPage = o);
+
+            t.MeetingAddress = "http://www.nytimes.com";
+            t.SwitchPages.Execute(null);
+
+            Assert.IsNull(newPage);
+        }
+#endif
+    }
+    }
--- Test_MRUDatabase/ViewModels/t_StartPage.cs
+++ Test_MRUDatabase/ViewModels/t_StartPage.cs
@@ -15,55 +15,5 @@ namespace Test_MRUDatabase.ViewModels
             var t = new StartPageViewModel(ds);
         }
 
-        [TestMethod]
-        public void LoadNormalMeeting()
-        {
-            var ds = new dummyScreen();
-            var t = new StartPageViewModel(ds);
-
-            object newPage = null;
-            ds.Router.Navigate.Subscribe(o => newPage = o);
-
-            t.MeetingAddress = "https://indico.cern.ch/event/377091/";
-            t.SwitchPages.Execute(null);
-
-            Assert.IsNotNull(newPage);
-            Assert.IsInstanceOfType(newPage, typeof(MeetingPageViewModel));
-        }
-
-#if false
-        [TestMethod]
-        public void LoadBadURL()
-        {
-            var ds = new dummyScreen();
-            var t = new StartPageViewModel(ds);
-
-            object newPage = null;
-            ds.Router.Navigate.Subscribe(o => newPage = o);
-
-            t.MeetingAddress = "http://www.nytimes.com";
-            t.SwitchPages.Execute(null);
-
-            Assert.IsNull(newPage);
-        }
-#endif
-
-        [TestMethod]
-        public void LoadCategory()
-        {
-            var ds = new dummyScreen();
-            var t = new StartPageViewModel(ds);
-
-            object newPage = null;
-            ds.Router.Navigate.Subscribe(o => newPage = o);
-
-            t.MeetingAddress = "https://indico.cern.ch/export/categ/1l12.ics?from=-7d";
-            t.SwitchPages.Execute(null);
-
-            Assert.IsNotNull(newPage);
-            Assert.IsInstanceOfType(newPage, typeof(CategoryPageViewModel));
-        }
-
-
     }
 }
